f348adf461752374807c1b409111513ec3df192f
neo-michael <s222074@student.uek.krakow.pl>
2025-03-10 23:03:47 +0100

    Formating..., added error-container and form.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,12 +7,12 @@ from flask import render_template, redirect, url_for
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev",
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -22,15 +22,14 @@ def create_app(test_config=None):
         pass
 
     # Route index to the correct view
-    @app.route('/')
+    @app.route("/")
     def index():
         return redirect(url_for("description.index"))
 
-
     from . import description
-    app.register_blueprint(description.bp)
-
     from . import product
+
+    app.register_blueprint(description.bp)
     app.register_blueprint(product.bp)
 
     return app
